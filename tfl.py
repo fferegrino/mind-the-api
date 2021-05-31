c5834e6874e12f09a5d6b42d76fa5ab2def9a00b
Antonio Feregrino <antonio.feregrino@gmail.com>
2021-05-31 07:16:14 +0100

    Turn code into Typer

--- tfl.py
+++ tfl.py
@@ -1,29 +1,27 @@
 import requests
+import typer
 import os
-import sys
 
 BASE_URL = "https://api.tfl.gov.uk"
 APP_KEY = os.environ["TFL_APP_KEY"]
 
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print("No option selected")
-        exit(1)
+main_app = typer.Typer()
 
-    if sys.argv[1] == "lines":
-        if len(sys.argv) != 3:
-            print("No action selected")
-            exit(1)
-        action = sys.argv[2]
-        if action == "status":
-            response = requests.get(BASE_URL + "/Line/Mode/tube/Status")
-            if response.status_code == 200:
-                data = response.json()
-                for line in data:
-                    print(line["name"])
-                    for status in line["lineStatuses"]:
-                        print("\t", status["statusSeverityDescription"])
+lines_app = typer.Typer()
 
-        else:
-            print("The action you selected is not an action under lines")
-            exit(1)
+
+@lines_app.command()
+def status():
+    response = requests.get(BASE_URL + "/Line/Mode/tube/Status")
+    if response.status_code == 200:
+        data = response.json()
+        for line in data:
+            print(line["name"])
+            for status in line["lineStatuses"]:
+                print("\t", status["statusSeverityDescription"])
+
+
+main_app.add_typer(lines_app, name="lines")
+
+if __name__ == "__main__":
+    main_app()
